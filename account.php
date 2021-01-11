afcedcb938af80f785d0dd3f5f4be073f2d3ea40
yashsaxena9597 <yashsaxena9597@gmail.com>
2021-01-11 16:44:26 +0530

    Commit

--- account.php
+++ account.php
@@ -39,52 +39,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 				<nav class="navbar navbar-default">
 					<div class="container-fluid">
 			<!-- Brand and toggle get grouped for better mobile display -->
-						<div class="navbar-header">
-							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-								<i class="sr-only">Toggle navigation</i>
-								<i class="icon-bar"></i>
-								<i class="icon-bar"></i>
-								<i class="icon-bar"></i>
-							</button>				  
-							<div class="navbar-brand">
-								<h1><a href="index.html">Planet Hosting</a></h1>
-							</div>
-						</div>
-
-			<!-- Collect the nav links, forms, and other content for toggling -->
-						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-							<ul class="nav navbar-nav">
-								<li><a href="index.html">Home <i class="sr-only">(current)</i></a></li>
-								<li><a href="about.html">About</a></li>
-								<li class="dropdown active">
-									<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Pages<i class="caret"></i></a>
-										<ul class="dropdown-menu">
-											<li><a href="blog.html">Blog</a></li>
-											<li><a href="pricing.html">Pricing</a></li>
-											<li><a href="faq.html">FAQ's</a></li>
-											<li><a href="testimonials.html">Testimonials</a></li>
-											<li><a href="history.html">History</a></li>
-											<li><a href="support.html">Support</a></li>
-											<li><a href="templatesetting.html">Template setting</a></li>
-											<li><a href="login.html">Login</a></li>
-											<li><a href="portfolio.html">Portfolio</a></li>
-										</ul>
-									</li>
-								<li><a href="services.html">Services</a></li>
-								<li class="dropdown">
-									<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Hosting<i class="caret"></i></a>
-									<ul class="dropdown-menu">
-										<li><a href="linuxhosting.html">Linux hosting</a></li>
-										<li><a href="wordpresshosting.html">WordPress Hosting</a></li>
-										<li><a href="windowshosting.html">Windows Hosting</a></li>
-										<li><a href="cmshosting.html">CMS Hosting</a></li>
-									</ul>			
-								</li>
-								<li><a href="codes.html">Codes</a></li>
-								<li><a href="contact.html">Contact</a></li>
-							</ul>
-									  
-						</div><!-- /.navbar-collapse -->
+			<?php include 'commonnav.php'; ?> 
 					</div><!-- /.container-fluid -->
 				</nav>
 			</div>
---
+++ commonnav.php
@@ -0,0 +1,46 @@
+<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+								<i class="sr-only">Toggle navigation</i>
+								<i class="icon-bar"></i>
+								<i class="icon-bar"></i>
+								<i class="icon-bar"></i>
+							</button>				  
+							<div class="navbar-brand">
+								<h1><a href="index.html">Planet Hosting</a></h1>
+							</div>
+						</div>
+
+			<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+							<ul class="nav navbar-nav">
+								<li class="active"><a href="index.php">Home <i class="sr-only">(current)</i></a></li>
+								<li><a href="about.php">About</a></li>
+								<li class="dropdown">
+									<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Pages<i class="caret"></i></a>
+										<ul class="dropdown-menu">
+											<li><a href="blog.php">Blog</a></li>
+											<li><a href="pricing.php">Pricing</a></li>
+											<li><a href="faq.php">FAQ's</a></li>
+											<li><a href="testimonials.php">Testimonials</a></li>
+											<li><a href="history.php">History</a></li>
+											<li><a href="support.php">Support</a></li>
+											<li><a href="templatesetting.php">Template setting</a></li>
+											<li><a href="login.php">Login</a></li>
+											<li><a href="portfolio.php">Portfolio</a></li>
+										</ul>
+									</li>
+								<li><a href="services.php">Services</a></li>
+								<li class="dropdown">
+									<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Hosting<i class="caret"></i></a>
+									<ul class="dropdown-menu">
+										<li><a href="linuxhosting.php">Linux hosting</a></li>
+										<li><a href="wordpresshosting.php">WordPress Hosting</a></li>
+										<li><a href="windowshosting.php">Windows Hosting</a></li>
+										<li><a href="cmshosting.php">CMS Hosting</a></li>
+									</ul>			
+								</li>
+								<li><a href="codes.php">Codes</a></li>
+								<li><a href="contact.php">Contact</a></li>
+							</ul>
+									  
+						</div><!-- /.navbar-collapse -->
